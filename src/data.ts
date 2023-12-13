ab99f81d7ab2d1569f3c134a13e87a386291b41e
Schuyler Klaassen <schuyler.m.k@gmail.com>
2023-12-13 15:30:49 -0800

    Separate data from logic, new file for 'fileTree'.

---
+++ src/data.ts
@@ -0,0 +1,183 @@
+export const fileTree = {
+  children: [
+    {
+      name: "react_fileTree",
+      children: [
+        {
+          name: "node_modules",
+          children: [
+            {
+              name: "acorn",
+              children: [
+                {
+                  name: "bin",
+                  children: [
+                    {
+                      name: "acorn",
+                    },
+                  ],
+                },
+                {
+                  name: "dist",
+                  children: [
+                    {
+                      name: "acorn.d.mts",
+                    },
+                    {
+                      name: "acorn.d.ts",
+                    },
+                    {
+                      name: "acorn.js",
+                    },
+                    {
+                      name: "acorn.mjs",
+                    },
+                    {
+                      name: "bin.js",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              name: "chalk",
+              children: [
+                {
+                  name: "types",
+                  children: [
+                    {
+                      name: "index.d.ts",
+                    },
+                  ],
+                },
+                {
+                  name: "index.js",
+                },
+                {
+                  name: "index.js.flow",
+                },
+                {
+                  name: "license",
+                },
+                {
+                  name: "package.json",
+                },
+                {
+                  name: "readme.md",
+                },
+                {
+                  name: "template.js",
+                },
+              ],
+            },
+            {
+              name: "escalade",
+              children: [
+                {
+                  name: "dist",
+                  children: [
+                    {
+                      name: "index.js",
+                    },
+                    {
+                      name: "index.mjs",
+                    },
+                  ],
+                },
+                {
+                  name: "sync",
+                  children: [
+                    {
+                      name: "index.d.ts",
+                    },
+                    {
+                      name: "index.js",
+                    },
+                    {
+                      name: "index.mjs",
+                    },
+                  ],
+                },
+                {
+                  name: "index.d.ts",
+                },
+                {
+                  name: "license",
+                },
+                {
+                  name: "package.json",
+                },
+                {
+                  name: "readme.md",
+                },
+              ],
+            },
+            {
+              name: "ms",
+              children: [
+                {
+                  name: "index.js",
+                },
+                {
+                  name: "license.md",
+                },
+                {
+                  name: "package.json",
+                },
+                {
+                  name: "readme.md",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "src",
+          children: [
+            {
+              name: "assets",
+              children: [
+                {
+                  name: "react.svg",
+                },
+              ],
+            },
+            {
+              name: "App.css",
+            },
+            {
+              name: "App.jsx",
+            },
+            {
+              name: "index.css",
+            },
+            {
+              name: "main.jsx",
+            },
+          ],
+        },
+        {
+          name: ".eslintrc.cjs",
+        },
+        {
+          name: ".gitignore",
+        },
+        {
+          name: "index.html",
+        },
+        {
+          name: "package-lock.json",
+        },
+        {
+          name: "package.json",
+        },
+        {
+          name: "README.md",
+        },
+        {
+          name: "vite.config.js",
+        },
+      ],
+    },
+  ],
+};
